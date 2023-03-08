65d1eea8bad551a96d9b8d54c33e0b4e2dd65bdd
tuant <phantuantai7293@gmail.com>
2023-03-08 15:42:28 +0700

    fix responsive 2

--- src/components/trustedBy/TrustedBy.jsx
+++ src/components/trustedBy/TrustedBy.jsx
@@ -3,29 +3,68 @@ import "./TrustedBy.scss";
 
 const TrustedBy = () => {
     return (
+        // <div className="trustedBy">
+        //     <div className="container mx-auto flex items-center justify-center gap-x-4 h-full">
+        //         <span className="hidden md:block">Trusted by:</span>
+        //         <img
+        //             src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook2x.188a797.png"
+        //             alt=""
+        //             className="max-w-[60px] w-auto"
+        //         />
+        //         <img
+        //             src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google2x.06d74c8.png"
+        //             alt=""
+        //             className="max-w-[60px] w-auto"
+        //         />
+        //         <img
+        //             src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/netflix2x.887e47e.png"
+        //             alt=""
+        //             className="max-w-[60px] w-auto"
+        //         />
+        //         <img
+        //             src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg2x.6dc32e4.png"
+        //             alt=""
+        //             className="max-w-[60px] w-auto"
+        //         />
+        //         <img
+        //             src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal2x.22728be.png"
+        //             alt=""
+        //             className="max-w-[60px] w-auto"
+        //         />
+        //     </div>
+        // </div>
         <div className="trustedBy">
-            <div className="container mx-auto flex items-center justify-center gap-x-4 h-full">
-                <span className="hidden md:block">Trusted by:</span>
-                <img
-                    src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook2x.188a797.png"
-                    alt=""
-                />
-                <img
-                    src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google2x.06d74c8.png"
-                    alt=""
-                />
-                <img
-                    src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/netflix2x.887e47e.png"
-                    alt=""
-                />
-                <img
-                    src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg2x.6dc32e4.png"
-                    alt=""
-                />
-                <img
-                    src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal2x.22728be.png"
-                    alt=""
-                />
+            <div className="container mx-auto flex items-center justify-center gap-2 h-full">
+                <div className="hidden md:block">
+                    <span>Trusted by:</span>
+                </div>
+                <div className="grid grid-cols-5 gap-2">
+                    <img
+                        src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook2x.188a797.png"
+                        alt=""
+                        className="h-[50px]"
+                    />
+                    <img
+                        src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google2x.06d74c8.png"
+                        alt=""
+                        className="h-[50px]"
+                    />
+                    <img
+                        src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/netflix2x.887e47e.png"
+                        alt=""
+                        className="h-[50px]"
+                    />
+                    <img
+                        src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg2x.6dc32e4.png"
+                        alt=""
+                        className="h-[50px]"
+                    />
+                    <img
+                        src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal2x.22728be.png"
+                        alt=""
+                        className="h-[50px]"
+                    />
+                </div>
             </div>
         </div>
     );
